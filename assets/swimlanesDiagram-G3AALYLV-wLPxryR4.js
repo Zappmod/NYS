d5d7cda0ddfcde735ccc9a3cc643410a9a84cb6d
Sophie-Harrison <sophie.harrison@ibm.com>
2026-09-09 08:53:38 -0400

    Initial commit

---
+++ assets/swimlanesDiagram-G3AALYLV-wLPxryR4.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-B8llLKiv.js";import{_ as a}from"./index.client-CaufwsFz.js";import"./chunk-5VM5RSS4-DgVM7pH6.js";import"./chunk-XXDRQBXY-CQGRF8MF.js";import"./chunk-VR4S4FIN-Dsz8g6_9.js";import"./chunk-32BRIVSS-DiA0m4Xc.js";import"./channel-JvS8dBsb.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
